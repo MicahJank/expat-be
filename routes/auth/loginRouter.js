cefe10f42c1de5622ad36e3ecf086d043661402e
MicahJank <MicahJank@users.noreply.github.com>
2020-09-19 11:50:26 -0500

    added verify body middleware to loginRouter

--- routes/auth/loginRouter.js
+++ routes/auth/loginRouter.js
@@ -6,7 +6,7 @@ const genToken = require('../../utils/genToken.js');
 // so i need to get that document here
 const Users = require('../../models/Users.js');
 
-router.post('/', (req, res) => {
+router.post('/', verifyBody, (req, res) => {
     const { password, email } = req.body;
     Users.findOne({ email })
     .then(user => {
@@ -22,5 +22,15 @@ router.post('/', (req, res) => {
         res.status(404).json({ message: 'Unable to find user in the database', error });
     })
 })
+
+
+
+function verifyBody(req, res, next) {
+    if (req.body.email && req.body.password) {
+        next()
+    } else {
+        res.status(401).json({ message: "Missing body information, make sure you are sending an email and password in the body request." });
+    }
+}
 
 module.exports = router;
